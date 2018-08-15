cf4c11aa6c6a7945254ad21e98193f1efb8a248d
dhananjaybhirud88 <37451342+dhananjaybhirud88@users.noreply.github.com>
2018-08-15 10:55:58 -0700

    - added header footer and main content (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {ContentComponent} from "./content/content.component";
 
 @Component({
   selector: 'app-root',
