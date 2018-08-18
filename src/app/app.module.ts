086b4e3609f1a73864e3a5cdd471b80dd32f7f7f
dhananjaybhirud88 <37451342+dhananjaybhirud88@users.noreply.github.com>
2018-08-18 07:45:38 -0700

    -  added routes and material kit files (#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+const appRoutes: Routes = [
+  { path: 'contact', component: ContactusComponent },
+  { path: 'home',      component: ContentComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
